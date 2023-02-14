87c7e8e27b012e442c64918ab88101530322dbe5
HariHaRAN <hariharanthiruvengadam334@gmail.com>
2023-02-14 17:35:10 +0530

    FRONT-BACK CONNECT

---
+++ SERVER/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ "limit" : "30mb" , extended : true}));
+app.use(bodyParser.urlencoded({ "limit" : "30mb" , extended : true}));
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://hari142:studywell333@cluster0.2ozzwep.mongodb.net/?retryWrites=true&w=majority'
+
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONNECTION_URL ,{useNewUrlParser : true , useUnifiedTopology : true })
+    .then(() => app.listen(PORT , () => console.log(`Server is rendering on ${PORT}`)))
+    .catch((error) =>  console.log(error.message));
